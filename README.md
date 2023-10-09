752b5c7b36462027c8384714a6bfcc3b91d7f544
hlxmh <mail.michaelhuang@gmail.com>
2023-10-09 02:50:14 -0400

    dynamic text box width

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 personal site
 =========
 - react + next.js 13 (app router)
+- node 18
 - tailwind
 - typescript
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import style from 'styles/app.module.css'
 
 export default function Home() {
   return (
-    <div className="flex justify-center h-[100%]">
-        <div  className="max-w-[95%] max-h-[95%] shadow-lg bg-black px-8 py-6 relative bg-opacity-40 mt-4 h-fit">
-          <h1 className="absolute top-0 left-3 animate-slow_blink">
+    <div className="flex h-[100%] px-[2.5%]">
+        <div  className="max-w-[100%] max-h-[95%] shadow-lg bg-black px-8 py-3 relative bg-opacity-40 mt-4 h-fit">
+          <div className="absolute top-3 left-3 animate-slow_blink text-5xl">
             &lceil;
-          </h1>
-          <h1 className="absolute bottom-0 right-3 animate-slow_blink">
+          </div>
+          <div className="absolute bottom-3 right-3 animate-slow_blink text-5xl">
             &rfloor;
-          </h1>
+          </div>
           <Typewriter></Typewriter>
         </div>
     </div>
