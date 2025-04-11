6f0e09ef7aeabdd4df19c4019ebf379a3b6562ec
hlxmh <mail.michaelhuang@gmail.com>
2025-04-11 10:57:41 -0400

    remove unnecessary client rendering

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,25 @@
-"use client";
-
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Header() {
-  return (
-    <header className="fixed z-10">
-      <ul>
-        <li>
-          <Link href="/">_index</Link>
-        </li>
-        <li>
-          <Link href="/project">_projects</Link>
-        </li>
-        <li>
-          <Link href="/sound">_music</Link>
-        </li>
-        <li>
-          <Link href="/img">_photography</Link>
-        </li>
-        <li>
-          <Link href="/txt">_thoughts</Link>
-        </li>
-      </ul>
-    </header>
-  );
+    return (
+        <header className="fixed z-10">
+            <ul>
+                <li>
+                    <Link href="/">_index</Link>
+                </li>
+                <li>
+                    <Link href="/project">_projects</Link>
+                </li>
+                <li>
+                    <Link href="/sound">_music</Link>
+                </li>
+                <li>
+                    <Link href="/img">_photography</Link>
+                </li>
+                <li>
+                    <Link href="/txt">_thoughts</Link>
+                </li>
+            </ul>
+        </header>
+    )
 }
